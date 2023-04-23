82599e1f6f0c01ae2b06a034269ff4119a34e1a1
Ketrin0108 <k-ketrin@list.ru>
2023-04-23 14:08:41 +0300

    доделала дз

--- test_2.py
+++ test_2.py
@@ -39,7 +39,56 @@ with open("weather.txt","a",encoding='utf-8') as write_file:
     write_file.write(str(c + "\n" + d ))
 
 
-#
+# Задание 3. Создать словарь в качестве  ключа будет 6-ти значное число (id),
+# а в качестве значений кортеж сосотящий из 2-ух элементов - имя(str), возраст (int).
+# Сделать около 5-6 элементов словаря. Записать данный словарь на диск в json-файл.
+
+import json
+
+data={
+    123456:
+        (('Kate',25),('Kira',20), ('Artem', 36), ('Lena', 22),('Inna', 40), ('Yulia', 29))
+}
+print(json.dumps(data))
+
+with open("data_file.json", "w+") as write_file:
+    json.dump(data, write_file)
+
+# Задание 4. Прочитать сохраненный json-файл и записать данные на диск в csv-файл,
+# первой строкой которого озаглавив каждый столбец и добавив новый столбец "телефон".
+import json
+
+with open("data_file.json", "r") as read_file:
+    data = json.load(read_file)
+print(data)
+
+
+import csv
+
+data=[['name', 'age', 'телефон'],
+      ['Kate','25'],
+      ['Kira','20'],
+      ['Artem', '36'],
+      ['Lena', '22'],
+      ['Inna', '40'],
+      ['Yulia', '29']]
+
+with open('data_file_new.csv', 'w+',encoding='utf-8') as f:
+    writer = csv.writer(f)
+    for row in data:
+        writer.writerow(row)
+
+with open('data_file_new.csv',encoding='utf-8') as f:
+    print(f.read())
+
+with open('data_file_new.csv',encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    list_=[]
+    for row in reader:
+        print(row)
+        list_.append(row)
+
+
 
 
 
