6a0831a1285a193f4f73560c4663a3e19b03675c
Ketrin0108 <k-ketrin@list.ru>
2023-05-05 13:58:46 +0300

    внесла изменения

--- test_10.py
+++ test_10.py
@@ -27,49 +27,72 @@
 # После того как прошлись по всем продуктам, вывести сообщение о том,
 # какие продукты были куплены, а какие не были
 
+from abc import ABC, abstractmethod
+
 class Product:
 
-    def __init__(self,namee,price,amount,list_of_product=None):
+    def __init__(self,id,name,price,amount):
+        self.id=id
         self.name=name
         self.price=price
         self.amount=amount
-        self.list_of_product=list_of_product
+        amount -= 1
+
+    @abstractmethod
+    def add_cost(self):
+        pass
+
+    @classmethod
+    def new_product(cls,id,name,price,amount):
+        return cls(id,name,price,amount)
+
 
 class Shop:
-    products = ['молоко', 'сыр', 'хлеб', 'рыба','мясо','творог','банан','мука']
+
     def __new__(cls): # new управляет созданием нового экземпляра класса
-        if not hasattr(cls, 'list_products'):
-            cls.list_products = super(Shop, cls).__new__(cls)
-        return cls.list_products
+        if not hasattr(cls, 'shop'):
+            cls.shop = super(Shop, cls).__new__(cls)
+        return cls.shop
 
+    def __init__(self,products):
+        self.products=products
 
-class Buyer:
-
-    def __init__(self,id,money,list_products):
-        self.id=id
-        self.money=money
-        self.list_products=list_products
-
-
-    @property
-    def basket(self):
-        return self._basket
-
-    @basket.setter
-    def basket(self, basket):
-        if list_products in Shop.products:
-            self._basket.append(list_products)
-        else:
-            raise Exception("Недостаточно денег")
 
 
 class Basket:
-    def __init__(self,id,list_products):
+    def __init__(self,id,new_basket):
+        self.id=id
+        self.new_basket=new_basket
         pass
 
+class Buyer:
 
-class Shopping_list:
-    pass
+    def __init__(self,id,money,list_product):
+        self.id=id
+        self.money=money
+        self.list_product=list_product
 
-buyer=Buyer(id='Kira',money=50,list_products=('творог',',банан','мука'))
-buyer.basket()
+        def make_basket(self):   #смотрит есть ли продукт в магазине и добовляет в новую корзину
+            for product in Shop.products:
+                if Product.name in Buyer.list_product:
+                self.new_basket.append(product)
+
+
+
+
+
+shop_1=Shop(products=('молоко','сыр', 'хлеб', 'рыба','мясо','творог','банан','мука'))
+print(shop_1.products)
+
+buyer=Buyer(id='user1',money=50,list_product=('творог',',банан','мука'))
+
+молоко =Product(id=1,name='Молоко',price=2,amount=5)
+сыр =Product(id=2,name='Сыр',price=5,amount=10)
+хдеб =Product(id=3,name='Хлеб',price=1,amount=10)
+рыба=Product(id=4,name='Рыба',price=10,amount=4)
+мясо=Product(id=5,name='Мясо',price=15,amount=6)
+творог=Product(id=6,name='Творог',price=6,amount=10)
+банан=Product(id=7,name='Банан',price=4,amount=10)
+мука=Product(id=8,name='Мука',price=3,amount=5)
+
+print(Shop(products))
